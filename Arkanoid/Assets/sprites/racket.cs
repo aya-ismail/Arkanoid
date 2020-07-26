8693acae2c80d55feb862753b1a6abab8cf5d942
aya-ismail <64666311+aya-ismail@users.noreply.github.com>
2020-07-26 23:38:11 +0200

    Arkanoid_updated

---
+++ Arkanoid/Assets/sprites/racket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racket : MonoBehaviour
+{
+    public int speed = 150;
+
+
+    void FixedUpdate()
+
+    { //get horizantal input 
+        float h = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+    
+    
+    
+        
+    }
+
+
+
